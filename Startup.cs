057ea3f54b037dae8e7692d0c5ae85abf00f5240
Makhanev Ruslan <ruslan.makhanev@extern.a1.at>
2022-03-27 15:49:24 +0300

    test

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ namespace LawyerTimeTracker
 {
     public class Startup
     {
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
