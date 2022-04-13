1501d1182c930becddf418ab351ea660b9edf79f
viktor-debre <viktor.debre2002@gmail.com>
2022-04-13 19:17:42 +0300

    ApplicationContext added as service in application

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LawyerTimeTracker.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,9 @@ namespace LawyerTimeTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("MariaDBConnectionString");
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
