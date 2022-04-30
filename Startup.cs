9d0e3fe7d847f5fedc0aa478305f160c4a7c1171
viktor-debre <viktor.debre2002@gmail.com>
2022-04-30 17:35:30 +0300

    Added authentication service and middleware in startup file.

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LawyerTimeTracker.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,7 +31,12 @@ namespace LawyerTimeTracker
             string connection = Configuration.GetConnectionString("MariaDBConnectionString");
             services.AddDbContextPool<ApplicationContext>(options =>
                 options.UseMySql(connection, ServerVersion.AutoDetect(connection) ));
-            
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+                });
             services.AddControllersWithViews(); 
         }
 
@@ -53,6 +59,7 @@ namespace LawyerTimeTracker
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
