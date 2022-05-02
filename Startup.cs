efd6519a844623f6b00b5cec0f2cfd4454e4df31
viktor-debre <viktor.debre2002@gmail.com>
2022-05-02 15:17:05 +0300

    Added path to redirect action for denying access by not proper role for user.

--- Startup.cs
+++ Startup.cs
@@ -24,8 +24,7 @@ namespace LawyerTimeTracker
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             string connection = Configuration.GetConnectionString("MariaDBConnectionString");
@@ -36,11 +35,11 @@ namespace LawyerTimeTracker
                 .AddCookie(options =>
                 {
                     options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+                    options.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
                 });
             services.AddControllersWithViews(); 
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
