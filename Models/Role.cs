60b016b67d334c143f6e8b992c3619c1270e3192
viktor-debre <viktor.debre2002@gmail.com>
2022-05-19 12:40:18 +0300

    Removed redundant code

--- Models/Role.cs
+++ Models/Role.cs
@@ -6,10 +6,5 @@ namespace LawyerTimeTracker.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<User> Users { get; set; }
-        public Role()
-        {
-            Users = new List<User>();
-        }
     }
 }
