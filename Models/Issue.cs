72e82ca60e34b5082800372b196c61496e2da2a9
viktor-debre <viktor.debre2002@gmail.com>
2022-05-25 16:06:32 +0300

    Added new model Issue

---
+++ Models/Issue.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LawyerTimeTracker.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string TypeOfTask { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int UserID { get; set; }
+    }
+}
